1ed095ef71150d3e2959e6e1f0cd4ca8f0ab320a
Hanna <annmurina1998@mail.ru>
2023-10-11 07:46:47 -0700

    fix

--- src/components/reusable/article/styledArticle.ts
+++ src/components/reusable/article/styledArticle.ts
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 export const Container  = styled.div`
-max-width: 1400px;
-    padding: 0 15px; 
+    max-width: 1400px;
+    padding: 160px 15px 60px 15px; 
     margin: 0 auto;
-    padding: 160px 0 60px 0;
 `
 
 export const Wrapper  = styled.div`
--- src/components/reusable/backBtn/backBtn.tsx
+++ src/components/reusable/backBtn/backBtn.tsx
@@ -1,13 +1,17 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 import { IconBtn } from "../iconBtn/iconBtn";
+import { useContext } from "react";
+import { ThemeContextType } from "../../../contexts/themeContext/types";
+import { ThemeContext } from "../../../contexts/themeContext/themeContext";
 
 export const GoBackBtn = () => {
     const navigate = useNavigate()
+    const themeContext = useContext<ThemeContextType>(ThemeContext)
 
     return (
         <div>
-            <IconBtn onClickFunc={() => navigate(-1)} size="xl" icon={faArrowLeft} />
+            <IconBtn color={themeContext.themeStyles.color} onClickFunc={() => navigate(-1)} size="xl" icon={faArrowLeft} />
         </div>
     )
 }
