865d2fad5c861cfaece870d6f0b46d93fe5fb0ab
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2023-10-26 15:31:25 +0300

    changed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import "./utils/browserRefresh/browserRefresh";
